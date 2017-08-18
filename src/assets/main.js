bdb2cc32ee37b7b48f1ae47ec202362648104668
Daniel Morgan <dmorgan@dasacquisition.com>
2017-08-18 11:43:11 -0500

    completed project

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,13 @@
 $(function() {
 
   // your code will go here
+  $.ajax({
+    url: 'https://www.codeschool.com/users/ndandan13.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      // handle response
+      response.courses.completed.map((x)=> $( "#badges" ).append( "<div class='course' ><h3>" + x.title + "</h3><img src=" + x.badge + " alt='badge of course'><a target='_blank' class='btn btn-primary' href=" + x.url + ">See Course</a></div>" ))
+    }
+  });
 
 });
